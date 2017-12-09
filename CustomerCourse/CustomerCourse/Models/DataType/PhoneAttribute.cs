a87179170db4f5c066303e59ed4533f86d8acea1
Tobey Yeh <tobey.yeh@gmail.com>
2017-12-09 21:23:31 +0800

    上傳Repository

---
+++ CustomerCourse/CustomerCourse/Models/DataType/PhoneAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace CustomerCourse.Models.DataType
+{
+    public partial class PhoneAttribute : DataTypeAttribute
+    {
+        public PhoneAttribute() : base(System.ComponentModel.DataAnnotations.DataType.Text)
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            if (value is String)
+            {
+                return Regex.IsMatch(value.ToString(), @"\d{4}-\d{6}");
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
